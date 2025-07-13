ee29a007f9ad1060b06a9b6a8175cfdd01acc7ac
Aldiyar Aisin <nesquikyey@gmail.com>
2025-07-13 15:24:17 +0500

    add create quiz page 3 step

--- src/app/(pages)/create-quiz/page.tsx
+++ src/app/(pages)/create-quiz/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import SectionWithHeader from "@/components/layouts/sectionWithHeader";
 import Input from "@/components/ui/input";
 import Textarea from "@/components/ui/textarea";
@@ -8,13 +10,20 @@ import IconButton from "@/components/ui/iconButton";
 
 import Arrow from "@/assets/arrow";
 import Cross from "@/assets/cross";
+import Triangle from "@/assets/triangle";
+import Share from "@/assets/share";
 
 export default function CreateQuiz() {
     return (
         <SectionWithHeader bigTitle="Создание квиза">
             <div>
                 {/* Main information */}
-                <h2 className="text-[3rem] font-extrabold mb-[3rem]">Заголовок этапа</h2>
+                <div className="mb-[3rem]">
+                    <h2 className="text-[3rem] font-extrabold mb-[1rem]">Заголовок этапа</h2>
+                    <p className="text-[2rem] ">
+                        Так будет выглядеть страница вашего квиза после публикации!
+                    </p>
+                </div>
                 {/* <div className="flex gap-[3rem]">
                     <div className="flex flex-col gap-[2rem] w-full">
                         <Input type="text" placeholder="Как он будет называться?" name="quiz-name" label="Название"/>
@@ -103,8 +112,40 @@ export default function CreateQuiz() {
                 {/* Questions */}
 
                 {/* Preview */}
-                <div>
-                    
+                <h2 className="text-[3rem] font-extrabold mb-[3rem]">Названия квиза</h2>
+                <div className="flex gap-[3rem]">
+                    <div className="max-w-[670px]">
+                        <Image
+                        src='/images/placeholder.png'
+                        width={670}
+                        height={380}
+                        alt="Quiz Cover"
+                        className="max-w-[100%] h-[380px] object-cover rounded-[1rem] border-light-2 border-[5px] mb-[3rem]"
+                        />
+                        <h3 className="text-[2.4rem] font-extrabold mb-[1.5rem]">Описание</h3>
+                        <p className="text-[2rem] font-normal">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget sollicitudin nunc, elementum vehicula odio. Cras mi turpis, volutpat in faucibus in, auctor vitae massa. Maecenas ac placerat lorem. Donec et orci at elit auctor tristique. Integer aliquet, turpis a tempor ultrices, mauris sem pellentesque purus, non blandit enim eros eget eros. Mauris ac turpis rhoncus arcu pharetra iaculis.
+                        </p>
+                    </div>
+                    <div className="bg-white border-[5px] border-gray p-[3rem] rounded-[1rem] w-full h-min">
+                        <div className="flex flex-col gap-[2.5rem] mb-[3rem]">
+                            <h4 className="text-[2.4rem] font-bold">💬 Категория</h4>
+                            <h4 className="text-[2.4rem] font-bold">👤 от Автора</h4>
+                            <h4 className="text-[2.4rem] font-bold">❓ Вопросы: 15</h4>
+                            <p className="text-gray text-[1.6rem]">Животные, Природа, Зоология</p>
+                        </div>
+                        <div className="flex gap-[1rem]">
+                            <div className="w-full">
+                                <Button type="yellow" styles="flex justify-center items-center gap-[0.5rem]">
+                                Начать
+                                <Triangle />
+                            </Button>
+                            </div>
+                            <IconButton type="blue">
+                                <Share />
+                            </IconButton>
+                        </div>
+                    </div>
                 </div>
                 {/* Preview */}
 
