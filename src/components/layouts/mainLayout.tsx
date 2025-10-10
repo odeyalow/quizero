c7ae9cbc70925d966253db96bce485346751fd57
Aldiyar <nesquikyey@gmail.com>
2025-10-10 14:31:13 +0500

    forgot to switch on the footer

--- src/components/layouts/mainLayout.tsx
+++ src/components/layouts/mainLayout.tsx
@@ -8,7 +8,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
             <main className="mt-[2rem] sm:mt-[4.5rem] relative h-max">
                 <Header />
                 {children}
-                {/* <Footer /> */}
+                <Footer />
             </main>
         </Providers>
     );
